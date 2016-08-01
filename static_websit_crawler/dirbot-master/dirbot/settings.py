2ed81ba5116fc8ec81267671a762c5a400a5e519
xtmin <daomin.wang@htouhui.com>
2016-08-01 18:28:48 +0800

    20160801

---
+++ static_websit_crawler/dirbot-master/dirbot/settings.py
@@ -0,0 +1,14 @@
+# Scrapy settings for dirbot project
+
+SPIDER_MODULES = ['dirbot.spiders']
+NEWSPIDER_MODULE = 'dirbot.spiders'
+DEFAULT_ITEM_CLASS = 'dirbot.items.CrawlerHeadItemBasket'
+
+ITEM_PIPELINES = {
+                  'dirbot.pipelines.MySQLStorePipeline': 100,
+                  }
+
+DOWNLOADER_MIDDLEWARES = {
+        'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware' : None,
+		'dirbot.UA.RotateUserAgentMiddleware': 400,
+    }
